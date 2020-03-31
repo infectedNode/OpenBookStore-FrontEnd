34d42659def35ab46ceb4beff035339537b49963
Shivam Sharma <shivam231198@gmail.com>
2020-03-31 17:29:21 +0530

    updates in heart animation and shadow

--- js/likes.js
+++ js/likes.js
@@ -3,9 +3,9 @@
 
 $('.books .book .cover .likes .heart').click(function(){
     let cur = $(this).html();
-    $(this).animate({fontSize: '+=1px'}, "fast");
-    $(this).animate({fontSize: '-=1px'}, "fast");
-    
+    $(this).animate({fontSize: '+=1px'}, 100);
+    $(this).animate({fontSize: '-=1px'}, 100);
+
     if(cur === '<i class="fas fa-heart"></i>') {
         $(this).html('<i class="far fa-heart"></i>');
     } else {
