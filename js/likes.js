6f4e65527a34ce49e90f231489bf7c7c0750228a
Shivam Sharma <shivam231198@gmail.com>
2020-03-31 17:21:59 +0530

    added animation to heart

--- js/likes.js
+++ js/likes.js
@@ -3,6 +3,9 @@
 
 $('.books .book .cover .likes .heart').click(function(){
     let cur = $(this).html();
+    $(this).animate({fontSize: '+=1px'}, "fast");
+    $(this).animate({fontSize: '-=1px'}, "fast");
+    
     if(cur === '<i class="fas fa-heart"></i>') {
         $(this).html('<i class="far fa-heart"></i>');
     } else {
