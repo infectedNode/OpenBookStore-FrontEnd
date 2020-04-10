7218c845662252bcb09df85321fc18c8e0d7adba
Shivam Sharma <shivam231198@gmail.com>
2020-04-10 17:06:19 +0530

    test

--- js/scroll.js
+++ js/scroll.js
@@ -8,3 +8,13 @@ $(window).on('scroll',function(){
 		$('.navbar .search').removeClass('active');
 	}
 });
+
+$('.recommendation .topics .topic .result .wrapper').scroll(function() {
+
+	if($('.recommendation .topics .topic .result .wrapper').scrollLeft()%245 != 0) {
+		let val = $('.recommendation .topics .topic .result .wrapper').scrollLeft();
+		$('.recommendation .topics .topic .result .wrapper').scrollLeft(val - val%245);
+	}
+
+	// console.log($('.recommendation .topics .topic .result .wrapper').scrollLeft());
+});
